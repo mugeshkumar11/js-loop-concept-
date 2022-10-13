3b636ae0bde069b20645866d822c910f85453d55
mugeshkumar <kmugesh11199@gmail.com>
2022-10-13 13:45:23 +0530

    add new file

---
+++ js/app.js
@@ -0,0 +1,59 @@
+// for loop
+
+//var a = 20;
+
+
+//for (let i=0; i<=a.length; i++){
+//console.log(i);
+//}
+
+//while loop
+
+//var b=11;
+
+//while(b<=20){
+ //   b++;
+
+  //  console.log(b);
+//}
+
+//program to display next five text
+
+//var n = 5;
+
+//for(let i=1; i<=n; i++){
+  //  console.log("mugesh");
+
+//}
+
+//sum of natural numbers
+
+//var sum = 0;
+
+//const n = 100;
+
+//for (let i=0; i<=n; i++){
+  //  sum+=i;
+//}
+//console.log("sum", sum);
+
+
+//var a = [10,20,30,40,50,60,70,80]
+
+//for(let i=0; i<=a.length; i++){
+  //  console.log("a",[i]);
+//}
+
+
+//var a = [10,20,30,40,50,60,70,80]
+
+//var max = 0;
+
+//for(let i=0; i<=a.length; i++){
+
+
+  //  if(max<a[i]){
+    //    max=a[i]
+    //}
+    //console.log(max)
+//}
